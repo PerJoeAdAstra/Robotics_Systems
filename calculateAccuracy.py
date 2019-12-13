1c3df25ba8f392a254986a0f48e4ad53fc45a317
Adam Sadiq <as16165@bristol.ac.uk>
2019-12-13 16:53:53 +0000

    Taken some readings, redo in lighter environments

---
+++ calculateAccuracy.py
@@ -0,0 +1,36 @@
+import csv
+
+no_correct = 0
+no_incorrect = 0
+
+total_offset = 0
+average_offset = 0
+
+
+def calculateAccuracy(reading, theta, actualAccuracy):
+    if (abs(float(reading) - float(actualAccuracy)) < 30):
+        return True
+    else:
+        return False
+
+def calculateAccuracy2(reading, theta, actualAccuracy, total_offset):
+    total_offset += abs(float(reading) - float(actualAccuracy))
+    return total_offset 
+
+
+#  float accuracy = 0;
+#   float objective_measurement = 0;
+#   for (int i = 0; i < measurements.size(); i++){
+#     objective_measurement = abs(270*sin(radsToDegs(measurements[i].theta)));
+#     Serial.println((String)objective_measurement + ", " + (String)measurements[i].distance_measured );
+#   }
+
+
+
+with open('measurements5.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        print(row)
+        total_offset = calculateAccuracy2(row[0], row[1], row[2], total_offset)
+    average_offset = float(total_offset)/36
+    print(average_offset)
---
+++ irproximity.h
@@ -0,0 +1,71 @@
+#ifndef _IRProximity_h
+#define _IRProximity_h
+
+class SharpIR {
+    public:
+        SharpIR(byte pin);
+        int getDistanceRaw();
+        float getDistanceInMM(float distance);
+        void calibrate(float distance);
+        float getDistanceCalibrated();
+
+    private:
+        byte pin;
+        float delta;
+        int NUM_OF_READINGS = 400;
+};
+
+SharpIR::SharpIR(byte _pin) {
+  pin = _pin;
+}
+
+int SharpIR::getDistanceRaw( ) {
+    return analogRead(pin);
+}
+
+
+void SharpIR::calibrate(float distance){
+  // user specificies distance it will be calibrated to
+  // difference is calculated over 100 reads
+  // return difference
+  float totalValues = 0;
+  float avgMM = 0;
+
+  for (int i = 0; i< NUM_OF_READINGS; i++){
+    totalValues += getDistanceRaw(); // get 100 raw readings
+  }
+
+  float avgValue = totalValues/NUM_OF_READINGS; // find the average
+  avgMM = getDistanceInMM(avgValue); //convert to MM
+  delta = distance - avgMM; // find difference
+  Serial.println("delta: " + (String)delta);
+  Serial.println("distance: " + (String)distance);
+  Serial.println("avgMM: " + (String)avgMM);
+}
+
+float SharpIR::getDistanceCalibrated(){
+  return getDistanceInMM(getDistanceRaw()) + delta;
+}
+
+
+/*
+ * This piece of code is quite crucial to mapping
+ * obstacle distance accurately, so you are encouraged
+ * to calibrate your own sensor by following the labsheet.
+ * Also remember to make sure your sensor is fixed to your
+ * Romi firmly and has a clear line of sight!
+ */
+float SharpIR::getDistanceInMM(float distance) {
+    
+    // map this to 0 : 5v range.
+    distance *= 0.0048;
+
+    const float exponent = (1/-0.616);
+    distance = pow( ( distance / 12.494 ), exponent);
+    distance *= 10; // to mm.
+       
+    return distance;
+}
+
+
+#endif
