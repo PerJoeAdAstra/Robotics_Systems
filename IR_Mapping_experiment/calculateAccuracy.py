90eef036f6745aedf50a9fc0e0de048212cf94fe
Adam Sadiq <adamsadiq@eduroam-190-241.nomadic.bris.ac.uk>
2019-12-17 19:14:20 +0000

    updating data reads

--- IR_Mapping_experiment/calculateAccuracy.py
+++ IR_Mapping_experiment/calculateAccuracy.py
@@ -1,12 +1,17 @@
 import csv
 import matplotlib.pyplot as plt
+import sys
+
 
 no_correct = 0
 no_incorrect = 0
 
 total_offset = 0
 average_offset = 0
+length = 0
 
+x = []
+y = []
 
 def calculateAccuracy(reading, theta, actualAccuracy):
     if (abs(float(reading) - float(actualAccuracy)) < 30):
@@ -14,28 +19,30 @@ def calculateAccuracy(reading, theta, actualAccuracy):
     else:
         return False
 
-def calculateAccuracy2(reading, theta, actualAccuracy, total_offset):
-    print(abs(float(reading) - float(actualAccuracy)), theta)
+def addDeviation(reading, theta, actualAccuracy, total_offset):
+    # print(abs(float(reading) - float(actualAccuracy)), theta)
     total_offset += abs(float(reading) - float(actualAccuracy))
     return total_offset 
 
 
-#  float accuracy = 0;
-#   float objective_measurement = 0;
-#   for (int i = 0; i < measurements.size(); i++){
-#     objective_measurement = abs(270*sin(radsToDegs(measurements[i].theta)));
-#     Serial.println((String)objective_measurement + ", " + (String)measurements[i].distance_measured );
-#   }
+fileName = sys.argv[1]
 
-
-x = []
-y = []
-
-with open('one-sensor-bad.csv', 'r') as csv_file:
+with open('{}.csv'.format(fileName), 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
+        #calculating deviation
+        total_offset = addDeviation(row[0],row[1],row[2], total_offset)
+        #calculating accuracy
+        if(calculateAccuracy(row[0],row[1],row[2])):
+            no_correct+=1
+        #add to pyplot
         y.append(float(row[0]) - 500)
         x.append(float(row[1]))
+        length+=1
+
+print("accuracy: ", float(no_correct)/float(length))
+print("deviation: ", float(total_offset-500)/float(length))
+
 
 plt.ylabel('Deviation (mm)')
 plt.xlabel('Theta (radians)')
