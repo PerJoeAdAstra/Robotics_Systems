6aef80dd1c1c013b9c7f352a0baa577b91bf7d12
Adam Sadiq <adamsadiq@eduroam-190-241.nomadic.bris.ac.uk>
2019-12-18 13:58:52 +0000

    experiments completed

--- IR_Mapping_experiment/calculateAccuracy.py
+++ IR_Mapping_experiment/calculateAccuracy.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import sys
 
 
-no_correct = 0
-no_incorrect = 0
+no_correct_15 = 0
+no_correct_30 = 0
 
 total_offset = 0
 average_offset = 0
@@ -13,8 +13,14 @@ length = 0
 x = []
 y = []
 
-def calculateAccuracy(reading, theta, actualAccuracy):
-    if (abs(float(reading) - float(actualAccuracy)) < 30):
+def calculateAccuracy15(reading, theta, actualAccuracy):
+    if (abs(float(reading) - float(actualAccuracy)) <= 15):
+        return True
+    else:
+        return False
+
+def calculateAccuracy30(reading, theta, actualAccuracy):
+    if (abs(float(reading) - float(actualAccuracy)) <= 30):
         return True
     else:
         return False
@@ -22,26 +28,29 @@ def calculateAccuracy(reading, theta, actualAccuracy):
 def addDeviation(reading, theta, actualAccuracy, total_offset):
     # print(abs(float(reading) - float(actualAccuracy)), theta)
     total_offset += abs(float(reading) - float(actualAccuracy))
-    return total_offset 
+    return total_offset
 
 
 fileName = sys.argv[1]
 
-with open('{}.csv'.format(fileName), 'r') as csv_file:
+with open('new-readings/{}.csv'.format(fileName), 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
         #calculating deviation
         total_offset = addDeviation(row[0],row[1],row[2], total_offset)
         #calculating accuracy
-        if(calculateAccuracy(row[0],row[1],row[2])):
-            no_correct+=1
+        if(calculateAccuracy15(row[0],row[1],row[2])):
+            no_correct_15+=1
+        if(calculateAccuracy30(row[0],row[1],row[2])):
+            no_correct_30+=1
         #add to pyplot
-        y.append(float(row[0]) - 500)
+        y.append(float(row[0]) - float(row[2]))
         x.append(float(row[1]))
         length+=1
 
-print("accuracy: ", float(no_correct)/float(length))
-print("deviation: ", float(total_offset-500)/float(length))
+print("Accuracy - 15mm window: ", float(no_correct_15)/float(length))
+print("Accuracy - 30mm window: ", float(no_correct_30)/float(length))
+print("Deviation: ", float(total_offset)/float(length))
 
 
 plt.ylabel('Deviation (mm)')
